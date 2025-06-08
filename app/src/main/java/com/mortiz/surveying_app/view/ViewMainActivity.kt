61bf828891a4bab12ca15acc192b2bfd04f119c6
MatiOrtiz <matiasnortiz.dev@gmail.com>
2025-06-08 20:09:53 -0300

    injector added

--- app/src/main/java/com/mortiz/surveying_app/view/ViewMainActivity.kt
+++ app/src/main/java/com/mortiz/surveying_app/view/ViewMainActivity.kt
@@ -2,8 +2,13 @@ package com.mortiz.surveying_app.view
 
 import android.app.Activity
 import android.os.Bundle
+import com.mortiz.surveying_app.R
+import com.mortiz.surveying_app.model.ModelInjector
+import com.mortiz.surveying_app.model.entities.Vote
+import com.mortiz.surveying_app.model.repository.VotesRepositoryImpl
+import com.mortiz.surveying_app.presentation.VotesPresenter
 
-class ViewMainActivity : Activity() {
+class ViewMainActivity : VotesView, Activity() {
     //Desarrollar una app que permita al usuario
 //votar entre tres opciones. La aplicación debe seguir el patrón MVP. La vista
 //(MainActivity) debe mostar los botones de votación, el presentador gestionar la lógica
@@ -15,9 +20,24 @@ class ViewMainActivity : Activity() {
 // Inyectar el modelo al presentador (inyección de dependencias).
 // La vista debe comunicarse solo con el presentador.
 // El presentador debe invocar actualizaciones en la vista mediante una interfaz
+    private lateinit var presenter: VotesPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        presenter = ModelInjector.init()
+
+    }
+
+    override fun showAllVotes() {
+        // Implementación para mostrar todos los votos
+
+    }
+
+    override fun showVotesByCategory(category: String) {
+
     }
 
 }
