008541546ae0d86e78304cd6d1432cc803f8539f
MatiOrtiz <matiasnortiz.dev@gmail.com>
2025-06-08 20:11:43 -0300

    presenter implemented

--- app/src/main/java/com/mortiz/surveying_app/presentation/VotesPresenter.kt
+++ app/src/main/java/com/mortiz/surveying_app/presentation/VotesPresenter.kt
@@ -1,2 +1,22 @@
 package com.mortiz.surveying_app.presentation
 
+import com.mortiz.surveying_app.model.entities.Vote
+import com.mortiz.surveying_app.model.repository.VotesRepository
+
+class VotesPresenter(votes:VotesRepository) {
+
+    private val votesRepository: VotesRepository = votes
+
+    fun addVote(vote: Vote) {
+        votesRepository.addVote(vote)
+    }
+
+    fun getAllVotes(): List<Vote> {
+        return votesRepository.getAllVotes()
+    }
+
+    fun getVotesByCategory(category: String): List<Vote> {
+        return votesRepository.getVotesByCategory(category)
+    }
+
+}
